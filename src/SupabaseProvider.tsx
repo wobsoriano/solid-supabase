e7f1888638b63f196af888aac6561617125c903e
Robert Soriano <sorianorobertc@gmail.com>
2021-08-16 20:39:31 +0800

    update provider

--- src/SupabaseProvider.tsx
+++ src/SupabaseProvider.tsx
@@ -1,25 +1,15 @@
-import { createClient, SupabaseClient, SupabaseClientOptions } from '@supabase/supabase-js'
+import type { SupabaseClient } from '@supabase/supabase-js'
 import { Component, createContext, useContext } from 'solid-js'
 
 const SupabaseContext = createContext<SupabaseClient>()
 
 interface Props {
-    config: {
-        supabaseUrl: string
-        supabaseKey: string
-        supabaseOptions?: SupabaseClientOptions
-    }
+    client: SupabaseClient
 }
 
 export const SupabaseProvider: Component<Props> = (props) => {
-    const {
-        supabaseUrl,
-        supabaseKey,
-        supabaseOptions
-    } = props.config
-    const supabase = createClient(supabaseUrl, supabaseKey, supabaseOptions)
     return (
-        <SupabaseContext.Provider value={supabase}>
+        <SupabaseContext.Provider value={props.client}>
             {props.children}
         </SupabaseContext.Provider>
     )
