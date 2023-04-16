855cffd011d48c24dfdef35748d65dda5326b71b
Michael サイトー 中村 Bashurov <accounts@saitonakamura.me>
2023-04-16 12:23:20 +0300

    feat: Allow providing client types via generics

--- src/primitives.ts
+++ src/primitives.ts
@@ -2,27 +2,59 @@ import type { AuthChangeEvent, AuthSession, SupabaseClient } from '@supabase/sup
 import { createEffect, onCleanup, useContext } from 'solid-js'
 import { SupabaseContext } from './SupabaseProvider'
 
-export const useSupabase = () => {
+export const useSupabase = <
+  Database = any,
+  SchemaName extends string & keyof Database = 'public' extends keyof Database
+    ? 'public'
+    : string & keyof Database,
+  Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+    ? Database[SchemaName]
+    : any
+>() => {
   const ctx = useContext(SupabaseContext)
 
   if (!ctx)
     throw new Error('useSupabase must be used within a SupabaseContext.Provider')
 
-  return ctx
+  return ctx as SupabaseClient<Database, SchemaName, Schema>
 }
 
-export function useSupabaseAuth(): SupabaseClient['auth'] {
-  const supabase = useSupabase()
+export function useSupabaseAuth<
+  Database = any,
+  SchemaName extends string & keyof Database = 'public' extends keyof Database
+    ? 'public'
+    : string & keyof Database,
+  Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+    ? Database[SchemaName]
+    : any
+>(): SupabaseClient<Database, SchemaName, Schema>['auth'] {
+  const supabase = useSupabase<Database, Schema, SchemaName>()
   return supabase.auth
 }
 
-export function useSupabaseStorage(): SupabaseClient['storage'] {
-  const supabase = useSupabase()
+export function useSupabaseStorage<
+  Database = any,
+  SchemaName extends string & keyof Database = 'public' extends keyof Database
+    ? 'public'
+    : string & keyof Database,
+  Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+    ? Database[SchemaName]
+    : any
+>(): SupabaseClient<Database, SchemaName, Schema>['storage'] {
+  const supabase = useSupabase<Database, SchemaName, Schema>()
   return supabase.storage
 }
 
-export function useSupabaseFrom(): SupabaseClient['from'] {
-  const supabase = useSupabase()
+export function useSupabaseFrom<
+  Database = any,
+  SchemaName extends string & keyof Database = 'public' extends keyof Database
+    ? 'public'
+    : string & keyof Database,
+  Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+    ? Database[SchemaName]
+    : any
+>(): SupabaseClient<Database, SchemaName, Schema>['from'] {
+  const supabase = useSupabase<Database, SchemaName, Schema>()
   return supabase.from
 }
 
@@ -32,8 +64,16 @@ interface OnAuthStateChangeOptions {
   autoDispose?: boolean
 }
 
-export function useOnAuthStateChange(callback: AuthChangeHandler, options: OnAuthStateChangeOptions = { autoDispose: true }): void {
-  const client = useSupabase()
+export function useOnAuthStateChange<
+  Database = any,
+  SchemaName extends string & keyof Database = 'public' extends keyof Database
+    ? 'public'
+    : string & keyof Database,
+  Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+    ? Database[SchemaName]
+    : any
+>(callback: AuthChangeHandler, options: OnAuthStateChangeOptions = { autoDispose: true }): void {
+  const client = useSupabase<Database, SchemaName, Schema>()
 
   const { data: authListener } = client.auth.onAuthStateChange((event, session) => {
     callback(event, session)
